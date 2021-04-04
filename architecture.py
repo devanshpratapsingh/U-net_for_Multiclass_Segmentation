1037572ad08dbc827111a5fa404c5ed96e0567e0
Devansh Pratap Singh <53232548+devanshpratapsingh@users.noreply.github.com>
2021-04-04 20:11:15 +0530

    Update architecture.py

--- architecture.py
+++ architecture.py
@@ -1,3 +1,3 @@
-from tensorflow.keras.layers import Input, conv2D, BatchNormalization,Activation, MaxPool2D, Upsample2D, Concataente
+from tensorflow.keras.layers import Input, conv2D, BatchNormalization,Activation, MaxPool2D, Upsample2D, Concatenate
 from tensorflow.keras.models import Model
 
